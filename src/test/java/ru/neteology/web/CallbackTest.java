98cd0e69164f0aa9e99af3fa5307994f59e0d626
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-10-25 21:26:14 +0300

    Chenge code3

--- src/test/java/ru/neteology/web/CallbackTest.java
+++ src/test/java/ru/neteology/web/CallbackTest.java
@@ -5,35 +5,43 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
     public static void setUpAll(){
-WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void beforeEash(){
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("- - disable-dev-shm-usage");
-        options.addArguments("- -sandbox");
-        options.addArguments("- -headless");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--sandbox");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http//localhost:9999");
     }
 
     @AfterEach
-    void tearDown(){
+    public void afterEach(){
         driver.quit();
         driver = null;
     }
     @Test
-    void shouldTestSomething(){
-        throw new UnsupportedOperationException();
+    public void shouldTestSomething(){
+        driver.findElement(By.cssSelector("[data-test-id='name') input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone') input")).sendKeys("+79139130099");
+        driver.findElement(By.cssSelector("[data-test-id='agreement')")).click();
+        driver.findElement(By.cssSelector("[data-test-id='button.button')")).click();
+        var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals("ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее времяю", actualText);
     }
 }
